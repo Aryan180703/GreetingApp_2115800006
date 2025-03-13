79fa9ff16951365c78b34297c4e1a662762d5bc9
Aryan Upadhyay <aryanupadhyay1807@gmail.com>
2025-03-14 00:16:21 +0530

    [Aryan Upadhyay] Add: UC6

---
+++ ModelLayer/Model/ResponseAllMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer.Model
+{
+    public class ResponseAllMessage
+    {
+        public string Email { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string Message { get; set; }
+    }
+}
